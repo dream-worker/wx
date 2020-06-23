19e6766cb98a33d3ea4da6d78293ad0ffd28325e
dream-worker <julieyang0718@163.com>
2020-06-23 10:07:50 +0800

    init

---
+++ miniprogram/pages/me/me.js
@@ -0,0 +1,42 @@
+// miniprogram/pages/me/me.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo: wx.getStorageSync("userInfo") || {}
+
+  },
+// detailPage
+  bindGetUserInfo(e){
+    let userInfo = e.detail.userInfo
+    wx.cloud.callFunction({
+      name:"login11",
+      success:(e)=>{
+        console.log(e)
+        userInfo.openid = e.result.openid
+        wx.setStorageSync('userInfo', userInfo)
+        this.setData({
+          userInfo
+        })
+
+      }
+    })
+    console.log(e)
+  },
+  bindLoginOut(){
+    let userInfo = {}
+    wx.setStorageSync('userInfo', userInfo)
+    this.setData({
+      userInfo
+    })
+  },
+  scanCode() {
+    wx.scanCode({
+      success(res) {
+        console.log(res)
+      }
+    })}
+  
+})
